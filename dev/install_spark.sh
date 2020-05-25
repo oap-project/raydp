1b81e861a636ba8be8cf81e12df33f0c4c29cd54
Xianyang Liu <xianyang.liu@intel.com>
2020-05-25 17:26:16 +0800

    fixes

--- dev/install_spark.sh
+++ dev/install_spark.sh
@@ -18,21 +18,11 @@ fi
 
 if ! [ -d ~/spark/spark-3.0.0-preview2-bin-hadoop2.7 ]; then
   pushd ~/spark/
-
   wget -cq https://downloads.apache.org/spark/spark-3.0.0-preview2/spark-3.0.0-preview2-bin-hadoop2.7.tgz
   tar zxf spark-3.0.0-preview2-bin-hadoop2.7.tgz
-
-  pushd spark-3.0.0-preview2-bin-hadoop2.7
-  ./build/mvn -DskipTests clean package
-
-  pushd python
-  python setup.py sdist
-  popd # python
-
-  popd # spark-3.0.0-preview2-bin-hadoop2.7
-  popd # spark
+  popd
 fi
 
 pushd ~/spark/spark-3.0.0-preview2-bin-hadoop2.7/python/
-python -m pip install dist/pyspark-3.1.0.dev0.tar.gz
+pip install .
 popd
