ad43fbcaa04ff791d78a977d3815ec1306a6a63b
Xianyang Liu <xianyang.liu@intel.com>
2020-05-28 14:12:06 +0800

    do not cache spark

--- dev/install_spark.sh
+++ dev/install_spark.sh
@@ -1,28 +1,8 @@
 #!/usr/bin/env bash
 
-reinstall=false
+wget -cq https://downloads.apache.org/spark/spark-3.0.0-preview2/spark-3.0.0-preview2-bin-hadoop2.7.tgz
+tar zxf spark-3.0.0-preview2-bin-hadoop2.7.tgz
 
-if [ $1 == "true" ]; then
-  reinstall=true
-fi
-
-if ! [ -d ~/spark ]; then
-  mkdir ~/spark
-fi
-
-if ${reinstall}; then
-  if [ -d ~/spark/spark-3.0.0-preview2-bin-hadoop2.7 ]; then
-    rm -rf ~/spark/spark-3.0.0-preview2-bin-hadoop2.7
-  fi
-fi
-
-if ! [ -d ~/spark/spark-3.0.0-preview2-bin-hadoop2.7 ]; then
-  pushd ~/spark/
-  wget -cq https://downloads.apache.org/spark/spark-3.0.0-preview2/spark-3.0.0-preview2-bin-hadoop2.7.tgz
-  tar zxf spark-3.0.0-preview2-bin-hadoop2.7.tgz
-  popd
-fi
-
-pushd ~/spark/spark-3.0.0-preview2-bin-hadoop2.7/python/
+pushd spark-3.0.0-preview2-bin-hadoop2.7/python/
 pip install .
 popd
