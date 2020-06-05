512f0a011af083fe347dca0f1eebacb6a88644a1
Xianyang Liu <xianyang.liu@intel.com>
2020-06-05 18:51:30 +0800

    profiling data loader

--- python/raydp/spark/torch/operator.py
+++ python/raydp/spark/torch/operator.py
@@ -27,3 +27,30 @@ class TrainingOperatorWithWarmUp(TrainingOperator):
         train_loader = self.train_loader
         plasma_store_path = ray.worker.global_worker.node.plasma_store_socket_name
         train_loader.sampler.resolve(plasma_store_path)
+
+    def train_epoch(self, iterator, info):
+        if self.timers is not None:
+            iterator = self._profile_data_loader(iterator)
+        super(TrainingOperatorWithWarmUp, self).train_epoch(iterator, info)
+
+    def _profile_data_loader(self, iterator):
+        outer = self
+
+        class DataLoaderWithProfile:
+            def __init__(self, iterator):
+                self._iterator = iterator
+                self._underlying_iterator = None
+
+            def __iter__(self):
+                if self._underlying_iterator is None:
+                    self._underlying_iterator = iter(self._iterator)
+                return self
+
+            def __next__(self):
+                with outer.timers.record("data_load"):
+                    next(self._underlying_iterator)
+
+            def __len__(self):
+                return len(iterator)
+
+        return DataLoaderWithProfile(iterator)
