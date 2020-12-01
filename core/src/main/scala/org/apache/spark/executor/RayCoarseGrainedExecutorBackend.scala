42ffb2598e45f72147b02695f8d01cda8ccd8e16
Xianyang Liu <xianyang.liu@intel.com>
2020-12-01 19:14:30 +0800

    set spark executor user.dir

--- core/src/main/scala/org/apache/spark/executor/RayCoarseGrainedExecutorBackend.scala
+++ core/src/main/scala/org/apache/spark/executor/RayCoarseGrainedExecutorBackend.scala
@@ -110,16 +110,18 @@ class RayCoarseGrainedExecutorBackend(
   }
 
   def createWorkingDir(appId: String): Unit = {
-    val file = new File(RayConfig.getInstance().sessionDir, appId)
+    // create the application dir
+    val app_dir = new File(RayConfig.getInstance().sessionDir, appId)
     var remainingTimes = 3
     var continue = true
     while (continue && remainingTimes > 0) {
       try {
-        file.mkdir()
-        continue = !file.exists()
+        app_dir.mkdir()
+        continue = !app_dir.exists()
         remainingTimes -= 1
         if (remainingTimes > 0) {
-          logError(s"Create ${file.getAbsolutePath} failed, remaining times: ${remainingTimes}")
+          logError(s"Create application dir: ${app_dir.getAbsolutePath} failed, " +
+            s"remaining times: ${remainingTimes}")
         }
       } catch {
         case e: SecurityException =>
@@ -127,36 +129,36 @@ class RayCoarseGrainedExecutorBackend(
       }
     }
 
-    if (file.exists()) {
-      if (file.isFile) {
+    if (app_dir.exists()) {
+      if (app_dir.isFile) {
         throw new RayDPException(
-          s"Expect ${file.getAbsolutePath} is a directory, however it is a file")
+          s"Expect ${app_dir.getAbsolutePath} is a directory, however it is a file")
       }
     } else {
-      throw new RayDPException(s"Create ${file.getAbsolutePath} failed after 3 times trying")
+      throw new RayDPException(s"Create application dir: ${app_dir.getAbsolutePath} failed " +
+        s"after 3 times trying")
     }
 
-    workingDir = file.getCanonicalFile
+    val executor_dir = new File(app_dir.getCanonicalPath, executorId)
+    if (executor_dir.exists()) {
+      throw new RayDPException(
+        s"Create ${executorId} working dir: ${executor_dir.getAbsolutePath} failed because " +
+          s"it existed already")
+    }
+    executor_dir.mkdir()
+    if (!executor_dir.exists()) {
+      throw new RayDPException(s"Create ${executorId} working dir: " +
+        s"${executor_dir.getAbsolutePath} failed")
+    }
+
+    workingDir = executor_dir.getCanonicalFile
   }
 
   def setUserDir(): Unit = {
     assert(workingDir != null && workingDir.isDirectory)
-    val file = new File(workingDir, executorId)
-    if (file.exists()) {
-      throw new RayDPException(
-        s"Create ${executorId} working dir failed because it existed already")
-    }
-    try {
-      file.mkdir()
-    } catch {
-      case e: Exception => throw e
-    }
-
-    if (!file.exists()) {
-      throw new RayDPException(s"Create ${executorId} working dir failed")
-    }
-    System.setProperty("user.dir", file.getAbsolutePath)
-    System.setProperty("java.io.tmpdir", file.getAbsolutePath)
+    System.setProperty("user.dir", workingDir.getAbsolutePath)
+    System.setProperty("java.io.tmpdir", workingDir.getAbsolutePath)
+    logInfo(s"Set user.dir to ${workingDir.getAbsolutePath}")
   }
 
   private def serveAsExecutor(
@@ -205,6 +207,12 @@ class RayCoarseGrainedExecutorBackend(
       val env = SparkEnv.createExecutorEnv(driverConf, executorId, nodeIp,
         nodeIp, cores, cfg.ioEncryptionKey, isLocal = false)
 
+      // set the tmp dir for the executor, it will be deleted when executor stop
+      val workerTmpDir = new File(workingDir, "_tmp")
+      workerTmpDir.mkdir()
+      assert(workerTmpDir.exists() & workerTmpDir.isDirectory)
+      SparkEnv.get.driverTmpDir = Some(workerTmpDir.getAbsolutePath)
+
       env.rpcEnv.setupEndpoint("Executor", backendCreateFn(env.rpcEnv, env, cfg.resourceProfile))
 
       env.rpcEnv.awaitTermination()
@@ -212,8 +220,8 @@ class RayCoarseGrainedExecutorBackend(
   }
 
   def redirectLog(): Unit = {
-    val logFile = Paths.get(workingDir.getAbsolutePath, executorId, s"executor${executorId}.out")
-    val errorFile = Paths.get(workingDir.getAbsolutePath, executorId, s"executor${executorId}.err")
+    val logFile = Paths.get(workingDir.getAbsolutePath, s"executor${executorId}.out")
+    val errorFile = Paths.get(workingDir.getAbsolutePath, s"executor${executorId}.err")
     logInfo(s"Redirect executor log to ${logFile.toString}")
     val appenders = LogManager.getRootLogger.getAllAppenders
     // There should be a console appender. Use its layout.
