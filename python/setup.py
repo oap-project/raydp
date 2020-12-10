e77012251e82f6b4ed281131800f4f270004d3ec
Xianyang Liu <xianyang.liu@intel.com>
2020-12-10 18:24:23 +0800

    fixes typo

--- python/setup.py
+++ python/setup.py
@@ -33,7 +33,7 @@ JARS_TARGET = os.path.join(TEMP_PATH, "jars")
 
 if len(JARS_PATH) == 0:
     print("Can't find core module jars, you need to build the jars with 'mvn clean package'"
-          " under core directly first.", file=sys.stderr)
+          " under core directory first.", file=sys.stderr)
     sys.exit(-1)
 else:
     JARS_PATH = JARS_PATH[0]
