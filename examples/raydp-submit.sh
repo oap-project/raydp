8550ea19f177770b612a02b4314ec8bd2f9b67be
Zhi Lin <zl1nn@outlook.com>
2021-05-11 09:28:21 +0000

    fix

--- examples/raydp-submit.sh
+++ examples/raydp-submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPARK_HOME=$(python -c "import os, pyspark; print(os.path.dirname(pyspark.__file__))")
+export SPARK_HOME=$(python -c "import os, pyspark; print(os.path.dirname(pyspark.__file__))")
 RAYDP_HOME=$(python -c "import os, raydp; print(os.path.dirname(raydp.__file__))")
 bin/raydp start localhost:7077
 bin/raydp submit --master localhost:7077 --conf spark.executor.cores=1 \
