de6148fe23710242f8506cad688172de24f41442
Zhi Lin <zl1nn@outlook.com>
2021-10-12 15:11:29 +0000

    update xgboost example

--- examples/xgboost_ray_nyctaxi.py
+++ examples/xgboost_ray_nyctaxi.py
@@ -27,9 +27,9 @@ spark.conf.set("spark.sql.session.timeZone", "UTC")
 data = nyc_taxi_preprocess(data)
 # Split data into train_dataset and test_dataset
 train_df, test_df = random_split(data, [0.9, 0.1], 0)
-# Convert spark dataframe into ML Dataset
-train_dataset = RayMLDataset.from_spark(train_df, 2, 32)
-test_dataset = RayMLDataset.from_spark(test_df, 2, 32)
+# Convert spark dataframe into ray dataset
+train_dataset = ray.data.from_spark(train_df, parallelism=2)
+test_dataset = ray.data.from_spark(test_df, parallelism=2)
 # Then convert them into DMatrix used by xgboost
 dtrain = RayDMatrix(train_dataset, label='fare_amount')
 dtest = RayDMatrix(test_dataset, label='fare_amount')
