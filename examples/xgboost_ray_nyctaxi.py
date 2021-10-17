2193d6219196cbfc1783cb5237c8a54d28a464a6
Zhi Lin <zl1nn@outlook.com>
2021-10-17 15:49:26 +0000

    upd

--- examples/xgboost_ray_nyctaxi.py
+++ examples/xgboost_ray_nyctaxi.py
@@ -11,7 +11,7 @@ from data_process import nyc_taxi_preprocess, NYC_TRAIN_CSV
 
 # connect to ray cluster
 # ray.init(address='auto')
-ray.init()
+ray.init(num_cpus=3)
 # After ray.init, you can use the raydp api to get a spark session
 app_name = "NYC Taxi Fare Prediction with RayDP"
 num_executors = 1
@@ -28,8 +28,8 @@ data = nyc_taxi_preprocess(data)
 # Split data into train_dataset and test_dataset
 train_df, test_df = random_split(data, [0.9, 0.1], 0)
 # Convert spark dataframe into ray dataset
-train_dataset = ray.data.from_spark(train_df, parallelism=1)
-test_dataset = ray.data.from_spark(test_df, parallelism=1)
+train_dataset = ray.data.from_spark(train_df)
+test_dataset = ray.data.from_spark(test_df)
 # Then convert them into DMatrix used by xgboost
 dtrain = RayDMatrix(train_dataset, label='fare_amount')
 dtest = RayDMatrix(test_dataset, label='fare_amount')
