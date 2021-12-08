240e18b4e02f3653e297da3cae07cf27fcc1b542
Bryan <bryan@Ubuntu-20.04>
2021-12-08 18:59:56 +0800

    add test case for data ownership transfer
    1. the main code change is to support spark dataframe to ray dataset
    conversion with data ownership transfer enabled
    
    2. by-product of data ownership transfer is that raydp JVM process can
    be killed after data is put into ray object store, while the data is
    still avaiable for subsequent use without throwing "data owner has died"
    exception. This is important from resource management point of view so
    that resource occupied by the JVM processes are now recycled back to Ray
    cluster.
    
    3. implementaion of such feature relies on Ray 1.9.0+, in simple words,
    it uses Ray namespace Java API and ray.put(data, _owner) API, and a
    Singleton Object Holder actor created in python side.

---
+++ python/raydp/tests/test_data_owner_transfer.py
@@ -0,0 +1,159 @@
+
+import sys
+import os
+import time
+
+import pytest
+import ray
+import raydp
+
+@pytest.mark.xfail(reason="data ownership transfer feature is not enabled")
+def test_fail_without_data_ownership_transfer():
+  """
+  参数设置
+  """
+
+  from raydp.spark.dataset import spark_dataframe_to_ray_dataset
+  
+  num_executor = 4
+
+  ray.shutdown()
+  raydp.stop_spark()
+
+  ray.init()
+  spark = raydp.init_spark(
+    app_name = "example",
+    num_executors = num_executor,
+    executor_cores = 1,
+    executor_memory = "800M"
+    )
+
+  test_file_path = os.path.abspath(__file__)
+  test_file_path = os.path.join(os.path.dirname(test_file_path),'data.txt')
+
+  df_train = spark.read.csv(test_file_path, sep=',', header=True, inferSchema=True)
+  # df_train = df_train.sample(False, 0.001, 42)
+
+  resource_stats = ray.available_resources()
+  cpu_cnt = resource_stats['CPU']
+
+  # convert data from spark dataframe to ray dataset,
+  # and transfer data ownership to dedicated Object Holder (Singleton)
+  df = spark_dataframe_to_ray_dataset(df_train, parallelism=4, _use_owner=True)
+
+  # display data
+  df.show(5)
+
+  # release resource by shutting down spark entirely
+  raydp.stop_spark()
+  ray.internal.internal_api.global_gc() # ensure GC kicked in
+  time.sleep(3)
+
+  # confirm that resources has been recycled
+  resource_stats = ray.available_resources()
+  assert resource_stats['CPU'] == cpu_cnt + num_executor
+
+  # confirm that data get lost (error thrown)
+  df.show(5)
+
+
+def test_data_ownership_transfer():
+  """
+  参数设置
+  """
+
+  from raydp.spark.dataset import spark_dataframe_to_ray_dataset
+  import numpy as np
+  
+  num_executor = 4
+
+  ray.shutdown()
+  raydp.stop_spark()
+
+  ray.init()
+  spark = raydp.init_spark(
+    app_name = "example",
+    num_executors = num_executor,
+    executor_cores = 1,
+    executor_memory = "800M"
+    )
+
+  test_file_path = os.path.abspath(__file__)
+  test_file_path = os.path.join(os.path.dirname(test_file_path),'data.txt')
+
+  df_train = spark.read.csv(test_file_path, sep=',', header=True, inferSchema=True)
+
+  resource_stats = ray.available_resources()
+  cpu_cnt = resource_stats['CPU']
+
+  # convert data from spark dataframe to ray dataset,
+  # and transfer data ownership to dedicated Object Holder (Singleton)
+  df = spark_dataframe_to_ray_dataset(df_train, parallelism=4, _use_owner=True)
+
+  # display data
+  df.show(5)
+
+  # release resource by shutting down spark Java process
+  raydp.stop_spark(del_obj_holder=False)
+  ray.internal.internal_api.global_gc() # ensure GC kicked in
+  time.sleep(3)
+
+  # confirm that resources has been recycled
+  resource_stats = ray.available_resources()
+  assert resource_stats['CPU'] == cpu_cnt + num_executor
+
+  # confirm that data is still available from object store!
+  df.show(5)
+
+  # sanity check the dataset is as functional as normal
+  df = df.to_dask()
+  assert np.isnan(df['Age'].mean().compute()) is not True
+   
+  # final clean up
+  raydp.stop_spark()
+  ray.shutdown()
+
+
+def test_api_compatibility():
+  num_executor = 4
+
+  ray.shutdown()
+  raydp.stop_spark()
+
+  ray.init()
+  spark = raydp.init_spark(
+    app_name = "example",
+    num_executors = num_executor,
+    executor_cores = 1,
+    executor_memory = "800M"
+    )
+
+  test_file_path = os.path.abspath(__file__)
+  test_file_path = os.path.join(os.path.dirname(test_file_path),'data.txt')
+
+  df_train = spark.read.csv(test_file_path, sep=',', header=True, inferSchema=True)
+
+  resource_stats = ray.available_resources()
+  cpu_cnt = resource_stats['CPU']
+
+  # check compatibility of ray 1.9.0 API: no data onwership transfer
+  df = ray.data.from_spark(df_train)
+  ray.internal.internal_api.global_gc() # ensure GC kicked in
+  time.sleep(3)
+
+  # confirm that resources is still being occupied
+  resource_stats = ray.available_resources()
+  assert resource_stats['CPU'] == cpu_cnt
+  
+  # final clean up
+  raydp.stop_spark()
+  ray.shutdown()
+
+
+if __name__ == '__main__':
+  sys.exit(pytest.main(["-v", __file__]))
+  
+  # test_api_compatibility()
+  # test_data_ownership_transfer()
+  # test_fail_without_data_ownership_transfer()
+
