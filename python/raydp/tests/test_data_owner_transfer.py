948fac62d8a19ec41055712683c71e19bc225be9
Bryan <bryan@Ubuntu-20.04>
2021-12-10 17:19:01 +0800

    minor change for CI

--- python/raydp/tests/test_data_owner_transfer.py
+++ python/raydp/tests/test_data_owner_transfer.py
@@ -12,11 +12,15 @@ def gen_test_data():
   from pyspark.sql.session import SparkSession
   s = SparkSession.getActiveSession()
  
-  data = [("ming", 20, 15552211521),
+  data = []
+  tmp = [("ming", 20, 15552211521),
           ("hong", 19, 13287994007),
           ("dave", 21, 15552211523),
           ("john", 40, 15322211523),
           ("wong", 50, 15122211523)]
+
+  for _ in range(10):
+    data += tmp
 
   rdd = s.sparkContext.parallelize(data)
   out = s.createDataFrame(rdd, ["Name", "Age", "Phone"])
@@ -54,10 +58,10 @@ def test_fail_without_data_ownership_transfer():
   cpu_cnt = resource_stats['CPU']
 
   # convert data from spark dataframe to ray dataset without data ownership transfer
-  df = spark_dataframe_to_ray_dataset(df_train, parallelism=4)
+  ds = spark_dataframe_to_ray_dataset(df_train, parallelism=4)
 
   # display data
-  df.show(5)
+  ds.show(5)
 
   # release resource by shutting down spark
   raydp.stop_spark()
@@ -69,7 +73,7 @@ def test_fail_without_data_ownership_transfer():
   assert resource_stats['CPU'] == cpu_cnt + num_executor
 
   # confirm that data get lost (error thrown)
-  df = df.to_dask()
+  ds.mean('Age')
 
 
 def test_data_ownership_transfer():
@@ -102,10 +106,10 @@ def test_data_ownership_transfer():
 
   # convert data from spark dataframe to ray dataset,
   # and transfer data ownership to dedicated Object Holder (Singleton)
-  df = spark_dataframe_to_ray_dataset(df_train, parallelism=4, _use_owner=True)
+  ds = spark_dataframe_to_ray_dataset(df_train, parallelism=4, _use_owner=True)
 
   # display data
-  df.show(5)
+  ds.show(5)
 
   # release resource by shutting down spark Java process
   raydp.stop_spark(del_obj_holder=False)
@@ -117,11 +121,8 @@ def test_data_ownership_transfer():
   assert resource_stats['CPU'] == cpu_cnt + num_executor
 
   # confirm that data is still available from object store!
-  df.show(5)
-
   # sanity check the dataset is as functional as normal
-  df = df.to_dask()
-  assert np.isnan(df['Age'].mean().compute()) is not True
+  assert np.isnan(ds.mean('Age')) is not True
    
   # final clean up
   raydp.stop_spark()
@@ -152,7 +153,7 @@ def test_api_compatibility():
   cpu_cnt = resource_stats['CPU']
 
   # check compatibility of ray 1.9.0 API: no data onwership transfer
-  df = ray.data.from_spark(df_train)
+  ds = ray.data.from_spark(df_train)
   ray.internal.internal_api.global_gc() # ensure GC kicked in
   time.sleep(3)
 
