8b4aa3b6b67a7691d7c7cd9523681418a9ad9dd2
Zhi Lin <zl1nn@outlook.com>
2022-08-29 08:59:55 +0000

    fix

--- examples/horovod_nyctaxi.py
+++ examples/horovod_nyctaxi.py
@@ -46,8 +46,7 @@ args = parser.parse_args()
 
 class NYC_Model(nn.Module):
     def __init__(self, cols):
-        super(NYC_Model, self).__init__()
-        
+        super().__init__()
         self.fc1 = nn.Linear(cols, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 64)
