1bb095f4812aad3eaaaaadbb9190f9407fbe0cd6
hezz <46663318+hezhaozhao-git@users.noreply.github.com>
2023-02-25 12:46:51 +0800

    fix test bug

--- python/raydp/tests/test_spark_to_dataset_recover_spark.py
+++ python/raydp/tests/test_spark_to_dataset_recover_spark.py
@@ -1,18 +1,43 @@
+from typing import Optional
+
 import ray
 import raydp
+from ray.data import Dataset
+from ray.data._internal.arrow_block import ArrowRow
+
+
+def from_spark(
+    df: "pyspark.sql.DataFrame", *,parallelism: Optional[int] = None
+    recover_ray_resources_from_spark=False
+) -> Dataset[ArrowRow]:
+    """Create a dataset from a Spark dataframe.
+    Args:
+        spark: A SparkSession, which must be created by RayDP (Spark-on-Ray).
+        df: A Spark dataframe, which must be created by RayDP (Spark-on-Ray).
+            parallelism: The amount of parallelism to use for the dataset.
+            If not provided, it will be equal to the number of partitions of
+            the original Spark dataframe.
+    Returns:
+        Dataset holding Arrow records read from the dataframe.
+    """
+    import raydp
+
+    return raydp.spark.spark_dataframe_to_ray_dataset(df,
+                                                      parallelism,
+                                                      recover_ray_resources_from_spark=recover_ray_resources_from_spark)
 
 ray.init()
 spark = raydp.init_spark(app_name="RayDP Example",
                          num_executors=2,
                          executor_cores=2,
-                         executor_memory="4GB")
+                         executor_memory="4G")
 
 
 
 # Spark Dataframe to Ray Dataset
 df1 = spark.range(0, 1000)
 raydp.stop_spark(False)
-ds1 = ray.data.from_spark(df1)
+ds1 = from_spark(df1, recover_ray_resources_from_spark=True)
 
 # Ray Dataset to Spark Dataframe
 ds2 = ray.data.from_items([{"id": i} for i in range(1000)])
