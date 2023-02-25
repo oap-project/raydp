39fa8536532a872174714dacdb6be0faf2390529
hezz <46663318+hezhaozhao-git@users.noreply.github.com>
2023-02-25 13:24:37 +0800

    add assert

--- python/raydp/tests/test_spark_to_dataset_recover_spark.py
+++ python/raydp/tests/test_spark_to_dataset_recover_spark.py
@@ -25,38 +25,39 @@ def from_spark(
     return raydp.spark.spark_dataframe_to_ray_dataset(df,
                                                       parallelism,
                                                       recover_ray_resources_from_spark=recover_ray_resources_from_spark)
+def test_():
+    
+    ray.init()
+    init_resources = ray.available_resources()
+    spark = raydp.init_spark(app_name="RayDP Example",
+                             num_executors=1,
+                             executor_cores=1,
+                             executor_memory="500M")
+    init_spark_resources = ray.available_resources()
+    assert (init_resources["CPU"] - init_spark_resources["CPU"]) == 1.0 
+    assert (init_resources["memory"] - init_spark_resources["memory"]) == 524288000.0
+    # Spark Dataframe to Ray Dataset
+    df1 = spark.range(0, 1000)
+    
+    ds1 = from_spark(df1, recover_ray_resources_from_spark=True)
+    
+    recover_ray_resources = ray.available_resources()
+    assert (recover_ray_resources["CPU"] - init_spark_resources["CPU"]) == 1.0 
+    assert (recover_ray_resources["memory"] - init_spark_resources["memory"]) == 524288000.0
+    
+    raydp.stop_spark(True)
+    stop_spark_resources = ray.available_resources()
+    assert (stop_spark_resources["CPU"] - init_spark_resources["CPU"]) == 1.0 
+    assert (stop_spark_resources["memory"] - init_spark_resources["memory"]) == 524288000.0
+    
+    # reinit spark
+    spark = raydp.init_spark(app_name="RayDP Example1",
+                             num_executors=1,
+                             executor_cores=1,
+                             executor_memory="500M")
 
-ray.init()
-spark = raydp.init_spark(app_name="RayDP Example",
-                         num_executors=2,
-                         executor_cores=2,
-                         executor_memory="4G")
+    # Ray Dataset to Spark Dataframe
+    ds2 = ray.data.from_items([{"id": i} for i in range(1000)])
+    df2 = ds2.to_spark(spark)
+    
 
-
-
-# Spark Dataframe to Ray Dataset
-df1 = spark.range(0, 1000)
-raydp.stop_spark(False)
-ds1 = from_spark(df1, recover_ray_resources_from_spark=True)
-
-# reinit spark
-
-spark = raydp.init_spark(app_name="RayDP Example1",
-                         num_executors=2,
-                         executor_cores=2,
-                         executor_memory="4G")
-
-
-# Ray Dataset to Spark Dataframe
-ds2 = ray.data.from_items([{"id": i} for i in range(1000)])
-df2 = ds2.to_spark(spark)
-
-
-"""
-my run res is:
-after init spark, ray available_resources is:  {'CPU': 2.0, 'memory': 2920706458.0, 'node:127.0.0.1': 1.0, 'object_store_memory': 2147483648.0}
-after convert spark df to ray dataset(use recover_ray_resources_from_spark is True),ray available_resources is:  {'CPU': 3.0, 'object_store_memory': 2147483648.0, 'node:127.0.0.1': 1.0, 'memory': 7215673754.0}
-after call raydp.stop_spark, ray available_resources is:  {'CPU': 4.0, 'memory': 7215673754.0, 'node:127.0.0.1': 1.0, 'object_store_memory': 2147483648.0}
-
-It can be seen that after adding the parameter recover_ray_resources_from_spark, the resources are indeed released
-"""
