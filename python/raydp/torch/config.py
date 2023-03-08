4155f89bc60e6307cf54b8b7dd166f6872d0dce2
KepingYan <keping.yan@intel.com>
2023-03-08 17:45:11 +0800

    Add torch_ccl version check

--- python/raydp/torch/config.py
+++ python/raydp/torch/config.py
@@ -2,6 +2,12 @@ from ray.train.torch.config import _TorchBackend
 from ray.train.torch.config import TorchConfig as RayTorchConfig
 from ray.train._internal.worker_group import WorkerGroup
 from dataclasses import dataclass
+import sys
+# The package importlib_metadata is in a different place, depending on the Python version.
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
 
 @dataclass
 class TorchConfig(RayTorchConfig):
@@ -12,8 +18,12 @@ class TorchConfig(RayTorchConfig):
 
 def ccl_import():
     try:
-        # pylint: disable=import-outside-toplevel
-        import oneccl_bindings_for_pytorch
+        ccl_version = importlib_metadata.version("oneccl_bind_pt")
+        if ccl_version >= "1.12":
+            # pylint: disable-all
+            import oneccl_bindings_for_pytorch
+        else:
+            import torch_ccl
     except ImportError as ccl_not_exist:
         raise ImportError(
             "Please install torch-ccl"
