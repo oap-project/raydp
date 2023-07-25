9a77b96e30a2ef83255c4ac0014236d087c105ae
Pang Wu <104795337+pang-wu@users.noreply.github.com>
2023-07-24 19:45:57 -0700

    Implement Spark executor node affinity using custom resource (#366)
    * Implement Spark executor node affinity using custom resource
    
    * reduce test flakeness
    
    * Update document

--- python/raydp/tests/conftest.py
+++ python/raydp/tests/conftest.py
@@ -16,14 +16,14 @@
 #
 
 import logging
+import subprocess
+import time
 
-import pytest
 import pyspark
-from pyspark.sql import SparkSession
+import pytest
 import ray
 import raydp
-import subprocess
-from ray.cluster_utils import Cluster
+from pyspark.sql import SparkSession
 
 
 def quiet_logger():
@@ -60,17 +60,19 @@ def spark_on_ray_small(request):
     else:
         ray.init(address=request.param)
     node_ip = ray.util.get_node_ip_address()
-    spark = raydp.init_spark("test", 1, 1, "500M", configs= {
+    spark = raydp.init_spark("test", 1, 1, "500M", configs={
         "spark.driver.host": node_ip,
         "spark.driver.bindAddress": node_ip
     })
 
     def stop_all():
         raydp.stop_spark()
+        time.sleep(5)
         ray.shutdown()
 
     request.addfinalizer(stop_all)
     return spark
+
 
 @pytest.fixture(scope="function", params=["local", "ray://localhost:10001"])
 def spark_on_ray_2_executors(request):
@@ -80,58 +82,18 @@ def spark_on_ray_2_executors(request):
     else:
         ray.init(address=request.param)
     node_ip = ray.util.get_node_ip_address()
-    spark = raydp.init_spark("test", 2, 1, "500M", configs= {
+    spark = raydp.init_spark("test", 2, 1, "500M", configs={
         "spark.driver.host": node_ip,
         "spark.driver.bindAddress": node_ip
     })
 
     def stop_all():
         raydp.stop_spark()
+        time.sleep(5)
         ray.shutdown()
 
     request.addfinalizer(stop_all)
     return spark
-
-
-@pytest.fixture(scope="function")
-def spark_on_ray_fraction_custom_resource(request):
-    ray.shutdown()
-    cluster = Cluster(
-        initialize_head=True,
-        head_node_args={
-            "num_cpus": 2
-        })
-    ray.init(address=cluster.address)
-
-    def stop_all():
-        raydp.stop_spark()
-        ray.shutdown()
-
-    request.addfinalizer(stop_all)
-
-
-@pytest.fixture(scope="function")
-def spark_on_ray_fractional_cpu(request):
-    ray.shutdown()
-    cluster = Cluster(
-        initialize_head=True,
-        head_node_args={
-            "num_cpus": 2
-        })
-
-    ray.init(address=cluster.address)
-
-    spark = raydp.init_spark(app_name="test_cpu_fraction",
-                             num_executors=1, executor_cores=3, executor_memory="500M",
-                             configs={"spark.ray.actor.resource.cpu": "0.1"})
-
-    def stop_all():
-        raydp.stop_spark()
-        ray.shutdown()
-
-    request.addfinalizer(stop_all)
-    return spark
-
 
 @pytest.fixture(scope='session')
 def custom_spark_dir(tmp_path_factory) -> str:
@@ -153,7 +115,8 @@ def custom_spark_dir(tmp_path_factory) -> str:
 
     import wget
 
-    wget.download(f"https://archive.apache.org/dist/spark/spark-{pyspark.__version__}/{spark_distribution}.{file_extension}",
-                  spark_distribution_file)
+    wget.download(
+        f"https://archive.apache.org/dist/spark/spark-{pyspark.__version__}/{spark_distribution}.{file_extension}",
+        spark_distribution_file)
     subprocess.check_output(['tar', 'xzvf', spark_distribution_file, '--directory', working_dir])
     return f"{working_dir}/{spark_distribution}"
