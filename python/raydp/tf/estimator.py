836e38aedafde6478ca6bf0286b8425483aa626c
Zhi Lin <zhi.lin@intel.com>
2024-04-12 14:51:52 +0000

    fix
    Signed-off-by: Zhi Lin <zhi.lin@intel.com>

--- python/raydp/tf/estimator.py
+++ python/raydp/tf/estimator.py
@@ -189,7 +189,7 @@ class TFEstimator(EstimatorInterface, SparkEstimatorInterface):
                 test_history = multi_worker_model.evaluate(eval_tf_dataset, callbacks=callbacks)
                 results.append(test_history)
         with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
-            multi_worker_model.save(os.path.join(temp_checkpoint_dir, save_format="tf"))
+            multi_worker_model.save(temp_checkpoint_dir, save_format="tf")
             checkpoint = Checkpoint.from_directory(temp_checkpoint_dir)
 
             session.report({}, checkpoint=checkpoint)
