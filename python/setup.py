e265eb7e0b89d447d77bd40afeb68314f3bf4d9b
Pang Wu <pang.wu@samsara.com>
2025-08-25 02:20:20 -0700

    exclude spark 3.x

--- python/setup.py
+++ python/setup.py
@@ -100,7 +100,7 @@ try:
         "psutil",
         "pyarrow >= 4.0.1",
         "ray >= 2.1.0",
-        "pyspark >= 3.1.1, <=4.0.0",
+        "pyspark >= 4.0.0",
         "netifaces",
         "protobuf > 3.19.5"
     ]
