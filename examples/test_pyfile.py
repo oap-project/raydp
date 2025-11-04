9da06fc3f1efce2be302afe6da2c42985ceaac2e
Pang Wu <pang.wu@samsara.com>
2025-11-04 01:14:32 -0800

    Support --py-files to distribute files to executors

---
+++ examples/test_pyfile.py
@@ -0,0 +1,8 @@
+"""
+Helper module for testing raydp-submit --py-files functionality.
+This module defines functions that will be used by the main.py script.
+"""
+
+def compute_sum(numbers):
+    """Compute sum to verify module functionality in Spark."""
+    return sum(numbers)
